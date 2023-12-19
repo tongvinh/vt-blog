c3344ba726a920adeb8e7e0683311f0aae464758
Vinh Tong <tongquangvinh2010@gmail.com>
2023-12-19 16:27:46 +0900

    Migration database version 1

---
+++ src/VTBlog.Data/VTBlogContextFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace VTBlog.Data
+{
+    public class VTBlogContextFactory : IDesignTimeDbContextFactory<VTBlogContext>
+    {
+        public VTBlogContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var builder = new DbContextOptionsBuilder<VTBlogContext>();
+            builder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            return new VTBlogContext(builder.Options);
+
+        }
+    }
+}
