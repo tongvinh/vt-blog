f69f149e449a99e52a8d907f3653078852e18211
Vinh Tong <tongquangvinh2010@gmail.com>
2024-01-08 14:54:30 +0700

    3.12. role management backend

---
+++ src/VTBlog.Core/Models/System/RoleDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using VTBlog.Core.Domain.Identity;
+
+namespace VTBlog.Core.Models.System
+{
+    public class RoleDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string DisplayName { get; set; }
+        public class AutoMapperProfiles: Profile
+        {
+            public AutoMapperProfiles()
+            {
+                CreateMap<AppRole, RoleDto>();
+            }
+        }
+    }
+}
